c407627083aecc73ead616f1d96d474dc94bbe93
Tim Harvey <harvey.tim@gmail.com>
2016-09-10 00:43:04 -0700

    MainActivity: add BT/BLE/Audio permission checks
    Move all our permission checks to the main activity so they fire all
    at once on first run.
    
    Signed-off-by: Tim Harvey <harvey.tim@gmail.com>

--- blocklybot/src/main/java/com/tharvey/blocklybot/MainActivity.java
+++ blocklybot/src/main/java/com/tharvey/blocklybot/MainActivity.java
@@ -2,6 +2,7 @@ package com.tharvey.blocklybot;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
 
     private static final int PERMISSION_REQUEST_RECORD_AUDIO = 1;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +30,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Android M Permission check
+            // Android M Permission checks
+            Log.d(TAG, "Checking Android M permissions");
+
+            // Audio recording required for voice recognition
             if (this.checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                final android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
                 builder.setTitle("This app needs audio recording permission");
                 builder.setMessage("Please grant audio recording permsision so this app can listen for voice commands.");
                 builder.setPositiveButton(android.R.string.ok, null);
@@ -41,8 +47,44 @@ public class MainActivity extends AppCompatActivity {
                 });
                 builder.show();
             }
+
+            // Location required for BLE scanning (yes, really it is... starting in Android 6.0)
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect Bluetooth Low Energy devices.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if (!adapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
     }
+
+    /* handle result of enable bluetooth request (above) */
+/* We don't really care right now if user chose to not enable Bluetooth
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == this.RESULT_CANCELED) {
+//            finish();
+            return;
+        }
+        onActivityResult(requestCode, resultCode, data);
+    }
+*/
 
     public void onClickBLE(View v) {
         final Intent intent = new Intent(this, BLEScanActivity.class);
